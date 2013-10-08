9225a12a93fa6405dca2c21c1d62c835971155c5
cym <cym@rock-chips.com>
2013-10-08 15:02:19 +0800

    ddrfreq:fix bug and add check vzalloc
    Conflicts:
    	arch/arm/plat-rk/ddr_freq.c

--- arch/arm/plat-rk/ddr_freq.c
+++ arch/arm/plat-rk/ddr_freq.c
@@ -300,10 +300,14 @@ static ssize_t video_state_write(struct file *file, const char __user *buffer,
 	uint32_t v_width=0,v_height=0,v_sync=0;
 	cookie_pot = buf;
 
-	if (count < 1) {
-                vfree(buf);
-		return count;
-        }
+	if(!buf)
+		return -ENOMEM;
+
+	if (count < 1){
+		vfree(buf);
+		return -EPERM;
+	}
+
 	if (copy_from_user(cookie_pot, buffer, count)) {
                 vfree(buf);
 		return -EFAULT;
