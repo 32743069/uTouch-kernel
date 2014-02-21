ecb4b5a76e8a7c23c6785f6c62415c313323ebbb
RuanJG <rjg@t-chip.com.cn>
2014-02-21 17:08:46 +0800

    Tchip->WIFI: nmc1000添加两个版本的驱动，P2P功能的驱动，定频测试的驱动，在内核配置时注意要选中相应的驱动

---
+++ drivers/net/wireless/nmi/nmc1000_7.2_spi_test/Nmc1000_Release7.2/src/svnrevision.h
@@ -0,0 +1,6 @@
+#ifndef SVNREV_H
+#define SVNREV_H
+#define NMC1000_DRIVER_VERSION "7.22"
+#define SVNREV "3660"
+#define SVNURL "svn://ssw.gotdns.org/SSW/sandbox/Wi-Fi/src/driver/head/platform/allwinner/chiphd/head/lichee/linux-3.3/drivers/net/wireless/nmi/Nmc1000_Release7.2"
+#endif
