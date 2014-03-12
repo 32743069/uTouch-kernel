23a6ff440bcf160efe2abdc94ba0e9f2417f637e
RuanJG <rjg@t-chip.com.cn>
2014-03-12 19:05:43 +0800

    Tchip->camera: 调整ro.sf.hwrotation＝270横屏配置后，gsensor与摄像头方向作调整

--- arch/arm/mach-rk3026/board-rk3026-86v.c
+++ arch/arm/mach-rk3026/board-rk3026-86v.c
@@ -632,7 +632,8 @@ static struct sensor_platform_data stk831x_info = {
 	.poll_delay_ms = 30,
         .init_platform_hw = stk831x_init_platform_hw,
 	#if defined(CONFIG_TCHIP_MACH_TR726C)
-	.orientation = { 0, 1, 0,1, 0, 0, 0, 0, -1},
+	//.orientation = { 0, 1, 0,1, 0, 0, 0, 0, -1},
+	.orientation = {1, 0, 0, 0, -1, 0, 0, 0, -1},
 	#else
 	.orientation = {-1, 0, 0, 0, 1, 0, 0, 0, -1},
 	#endif
