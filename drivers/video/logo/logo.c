93e23a32f4543b7c08fffe36f5715dce3257180d
RuanJG <rjg@t-chip.com.cn>
2014-03-17 09:29:24 +0800

    Tchip->LOGO: 添加客户logo

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -84,6 +84,10 @@ const struct linux_logo * __init_refok fb_find_logo(int depth)
 		/* Generic Linux logo */
 		logo = &logo_linux_clut224;
 #endif
+#ifdef CONFIG_LOGO_TCHIP_CUSTOMER_CLUT224
+		/* tchip customer logo */
+		logo = &logo_tchip_customer_clut224;
+#endif
 #ifdef CONFIG_LOGO_PIPO_CLUT224
 		/* Generic Linux logo */
 		logo = &logo_pipo_clut224;
